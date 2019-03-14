3973fc2c2044764ba4340c0b9499d4c24d26b61e
Tom Randles <randles.tom@gmail.com>
2019-03-14 21:05:08 +0000

    commit

---
+++ EAP_WS1/Pages/greeting.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EAP_WS1.Pages
+{
+    public class greetingModel : PageModel
+    {
+        public void OnGet()
+        {
+            if (DateTime.Now.Hour > 12)
+                message += ", good afternoon";
+            else
+                message += ", good morning";
+        }
+        public string message { get; set; } = "Hello";
+    }
+}
