6433d4a9caef8e7c7d36a0a9fb5f686df4087add
Tom Randles <randles.tom@gmail.com>
2019-03-19 13:21:52 +0000

    DataType exercises

---
+++ EAP_WS1/Pages/DataType.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EAP_WS1.Pages
+{
+    public class DataTypeModel : PageModel
+    {
+        [BindProperty]
+        public string message { get; set; } = "";
+
+        [BindProperty]
+        [DataType(DataType.Date)]
+        public DateTime   myDate { get; set; }
+
+        [BindProperty]
+        [DataType(DataType.PhoneNumber)]
+        public string phoneNumber { get; set; }
+
+        [BindProperty]
+        [DataType(DataType.Currency)]
+        public Decimal currency { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+        public void OnPost()
+        {
+            message = $"My DoB is: {myDate}; Savings are: {currency}; Phone number is: {phoneNumber}; ";
+        }
+    }
+}
